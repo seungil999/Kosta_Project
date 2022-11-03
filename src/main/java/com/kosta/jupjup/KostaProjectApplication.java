78441dd90239b56e9a48b8422032782db55e0642
forsomeday97 <tmajvm313@gmail.com>
2022-11-03 21:49:48 +0900

    Initial commit

---
+++ src/main/java/com/kosta/jupjup/KostaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kosta.jupjup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KostaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KostaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kosta/jupjup/KostaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.jupjup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KostaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
