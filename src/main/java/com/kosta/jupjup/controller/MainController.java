e49e68828d8d3d27070b0ac2863261dd8f5625e1
forsomeday97 <tmajvm313@gmail.com>
2022-11-06 17:29:08 +0900

    부트스트랩 적용
    ㅇㅇ

---
+++ src/main/java/com/kosta/jupjup/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.kosta.jupjup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+ 
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String main() {
+		
+		return "main";
+	}
+	
+	@GetMapping("/test")
+	public String main2() {
+		
+		return "testz";
+	}
+	
+	
+}
