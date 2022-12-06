5bc833f78fb7fd583add9ffd9102b9b6129f3d78
forsomeday97 <tmajvm313@gmail.com>
2022-12-06 13:45:58 +0900

    댓글/ 참여중인유저 프로필/모달
    ㅇㅇ

--- src/main/java/com/kosta/jupjup/vo/MateReplyVO.java
+++ src/main/java/com/kosta/jupjup/vo/MateReplyVO.java
@@ -14,4 +14,24 @@ public class MateReplyVO {
 	private Date replyDate;
 	private Date updateDate;
 	private String user_id;
+	
+	/** 유저 프로필 **/
+	private String username;
+
+	private String nickname;
+
+	private String id;
+
+	private String pwd;
+
+	private String email;
+
+	private String phone;
+
+	private String gender;
+	
+	private String profile;
+	
+	private String profile_open;
+	
 }
